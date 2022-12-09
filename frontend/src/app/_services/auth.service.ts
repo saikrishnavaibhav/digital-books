653ec21c289074c713e89a64e055815ab1e802be
saikrishnavaibhav <saikrishnavaibhav1234@gmail.com>
2022-12-09 22:17:53 +0530

    added dev environment, role selection while signup

--- frontend/src/app/_services/auth.service.ts
+++ frontend/src/app/_services/auth.service.ts
@@ -21,12 +21,13 @@ export class AuthService {
     }, httpOptions);
   }
 
-  register(userName: string, emailId: string, phoneNumber: String, password: string): Observable<any> {
+  register(userName: string, emailId: string, phoneNumber: String, password: string, roles: any): Observable<any> {
     return this.http.post(AUTH_API + 'sign-up', {
       userName,
       emailId,
       phoneNumber,
-      password
+      password,
+      roles
     }, httpOptions);
   }
 }
--- frontend/src/app/register/register.component.ts
+++ frontend/src/app/register/register.component.ts
@@ -6,12 +6,13 @@ import { AuthService } from '../_services/auth.service';
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent {
   form: any = {
     userName: null,
     emailId: null,
     phoneNumber: null,
-    password: null
+    password: null,
+    role: null
   };
   isSuccessful = false;
   isSignUpFailed = false;
@@ -19,13 +20,11 @@ export class RegisterComponent implements OnInit {
 
   constructor(private authService: AuthService) { }
 
-  ngOnInit(): void {
-  }
-
   onSubmit(): void {
-    const { userName, emailId, phoneNumber, password } = this.form;
-
-    this.authService.register(userName, emailId, phoneNumber, password).subscribe(
+    const { userName, emailId, phoneNumber, password, role } = this.form;
+    let roles = []
+    roles.push(role);
+    this.authService.register(userName, emailId, phoneNumber, password, roles).subscribe(
       data => {
         console.log(data);
         this.isSuccessful = true;
